35e929d2ae1aa4b234218e722871ca22dd20df89
Sean McKinney <seanamckinney8@gmail.com>
2022-04-11 16:22:34 -0500

    Initial Commit.  Take all numbers between 0 and 1000 that are divisible by 3 and 5, add them and print the answer to the console.

--- MultiplesOf3And5PlusSum/Program.cs
+++ MultiplesOf3And5PlusSum/Program.cs
@@ -10,6 +10,16 @@ namespace MultiplesOf3And5PlusSum
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            int sum = 0;
+            for (int counter = 1; counter <= 1000; counter++)
+            {
+                if (counter % 3 == 0 || counter % 5 == 0)
+                {
+                    sum += counter;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }//end main
+    }//end class
+}//end namespace
